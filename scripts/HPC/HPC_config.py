adcd92d7b9c953eb5bad472b976b5a82f44cd806
Simon Håkansson <simon@delusional.enterprises>
2025-10-23 16:24:43 +0200

    feat: add HPC configuration file with environment-specific settings

---
+++ scripts/HPC/HPC_config.py
@@ -0,0 +1,8 @@
+"""
+This config file contains configuration settings for the HPC environment.
+It is used to set up paths and parameters specific to the HPC cluster. 
+Please ensure to modify the settings according to your HPC cluster requirements.
+
+For local configurations, please refer to the config.yaml file located at the 
+root of the repository.   
+"""
